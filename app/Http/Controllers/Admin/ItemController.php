bf2ca11599c9cb71d5f586889418f687b2b49aa7
htet-aungkyaw <200138nr@yse-c.net>
2022-01-24 12:00:28 +0900

    finish

--- app/Http/Controllers/Admin/ItemController.php
+++ app/Http/Controllers/Admin/ItemController.php
@@ -4,17 +4,22 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
-    //
-    public function index()
-    {
-        return view('admin.item.index');
-    }
-
-    public function create()
-    {
+    public function create() {
         return view('admin.item.create');
     }
+
+   
+    public function add(Request $request)
+    {
+        
+        $posts = $request->all();
+        
+        Item::create($posts);
+        return redirect()->route('admin.item.index');
+    }
+
 }
--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,3 +1,15 @@
 <h2>商品入力</h2>
+<from action="{{ route('admin.item.add') }}" method="post">
+    @csrf
 
-<a href="{{ route('admin.item.index') }}">戻る</a>
+    <div>
+        <label for ="">{{_('Item Name') }}</label>
+        <input type ="text" name = "name">
+    </div>
+    <div>
+        <label for="">{{_('price')}}</label>
+        <input type="text" name="price">
+    </div>
+    <button>{{_('Upate')}}</button>
+<a href="{{ route('admin.item.index') }}">{{_('Back')}}</a>
+</from>
